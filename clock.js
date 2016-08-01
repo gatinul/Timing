3e778105ab8b828460d713a3ce20deb3be7a3beb
Your Name <13252716435@163.com>
2016-08-01 16:56:16 +0800

    easy timing device

---
+++ clock.js
@@ -0,0 +1,33 @@
+window.onload = function() {
+
+	var deadline = '2016-12-31';
+
+	function getTimeRemaining(endtime) {
+		var t = Date.parse(endtime) - Date.parse(new Date());
+		var seconds = Math.floor((t / 1000) % 60);
+		var minutes = Math.floor((t / 1000 / 60) % 60);
+		var hours = Math.floor((t / 1000 / 60 / 60) % 24);
+		var days = Math.floor((t / 1000 / 60 / 60 / 24));
+
+		return {
+			'total': t,
+			'days': days,
+			'hours': hours,
+			'minutes': minutes,
+			'seconds': seconds
+		};
+
+	}
+
+	function initializeClock(id, endtime) {
+		var clock = document.getElementById('clockdiv')
+		var timeinterval = setInterval(function() {
+			var t = getTimeRemaining(endtime);
+			clock.innerHTML = 'days' + t.days + '&lt;br&gt;' + 'hours' + t.hours + '&lt;br&gt;' + 'minutes' + t.minutes + '&lt;br&gt;' + 'seconds' + t.seconds;
+			if (t.total <= 0) {
+				clearInterval(timeinterval);
+			}
+		}, 1000)
+	}
+	initializeClock('clockdiv', deadline);
+};
