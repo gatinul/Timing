be2e78f3e514bbd42b6fc0f50c084d558835a2fa
Your Name <13252716435@163.com>
2016-08-02 09:11:47 +0800

    you hua xian shi

--- clock.js
+++ clock.js
@@ -25,9 +25,12 @@ window.onload = function() {
 			var t = getTimeRemaining(endtime);
 			var daysSpan = clock.querySelector('.days');
 			var hoursSpan = clock.querySelector('.hours');
-			var minutesSpan = clock.querySelector('minutes');
-			var secondsSpan = clock.querySelector('seconds');
+			var minutesSpan = clock.querySelector('.minutes');
+			var secondsSpan = clock.querySelector('.seconds');
 			daysSpan.innerHTML = t.days;
+			hoursSpan.innerHTML=('0'+t.hours).slice(-2);
+			minutesSpan.innerHTML=('0'+t.minutes).slice(-2);
+			secondsSpan.innerHTML=('0'+t.seconds).slice(-2);
 			if (t.total <= 0) {
 				clearInterval(timeinterval);
 			}
